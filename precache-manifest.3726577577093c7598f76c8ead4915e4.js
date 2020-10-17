2bce3b1256de4de90a50321857d71661db626c13
Inpyo Lee <mynameispyo@gmail.com>
2020-10-17 11:50:40 -0400

    Updates

---
+++ precache-manifest.3726577577093c7598f76c8ead4915e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1599dececab842384d37f303676598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d56d8c16e8f8851e917d",
+    "url": "/static/css/main.86fde8f1.chunk.css"
+  },
+  {
+    "revision": "d0c21ba9ab034c7e5e6e",
+    "url": "/static/js/2.d777dcb9.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/static/js/2.d777dcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56d8c16e8f8851e917d",
+    "url": "/static/js/main.9832b9e1.chunk.js"
+  },
+  {
+    "revision": "bd00b011b89de116e745",
+    "url": "/static/js/runtime-main.85d7b3f1.js"
+  }
+]);
